8d90dde3f260a47661038015ba9a1d7289ed3a99
THE JOKER <pavel11_06@mail.ru>
2021-07-25 16:42:37 +0800

    добавлена сущность Url, asset

---
+++ src/Entity/Url.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UrlRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UrlRepository::class)
+ */
+class Url
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $original_url;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $new_link;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOriginalUrl(): ?string
+    {
+        return $this->original_url;
+    }
+
+    public function setOriginalUrl(string $original_url): self
+    {
+        $this->original_url = $original_url;
+
+        return $this;
+    }
+
+    public function getNewLink(): ?string
+    {
+        return $this->new_link;
+    }
+
+    public function setNewLink(string $new_link): self
+    {
+        $this->new_link = $new_link;
+
+        return $this;
+    }
+}
---
+++ src/Repository/UrlRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Url;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Url|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Url|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Url[]    findAll()
+ * @method Url[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UrlRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Url::class);
+    }
+
+    /**
+     * @param string $value origin_url
+     * @return Url Returns an one Url object
+     */
+    public function findOneByOriginalUrl(string $value): ?Url
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.original_url = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
